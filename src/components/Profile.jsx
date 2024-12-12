3696c70913874e6de49b86fe70d26b7582bd0d0e
GIAHUY\GiaHuy <huypro99998@gmail.com>
2024-12-12 20:35:59 +0700

    final

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,7 +6,6 @@ function Profile() {
         <div className="profile">
             <h1>User Profile</h1>
             <p>Welcome to the user's profile page!</p>
-            {/* Thêm các thành phần cần thiết cho giao diện profile tại đây */}
         </div>
     );
 }
